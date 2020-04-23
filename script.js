d8820b415a8f71eaf0f4397ca072b94eb34ed778
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-04-23 10:56:28 +0500

    insert script

---
+++ script.js
@@ -0,0 +1,5 @@
+const postMain = document.getElementById('pos_main');
+const filSec = document.querySelector('.pos_sec_main');
+const filter = document.getElementById('filter');
+let limit = 5;
+let page = 1;
