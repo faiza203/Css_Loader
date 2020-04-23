b84d1f8f016f42d185ef741add94e2d040cda758
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-04-23 11:26:32 +0500

    completed

--- script.js
+++ script.js
@@ -1,5 +1,58 @@
-const postMain = document.getElementById('pos_main');
-const filSec = document.querySelector('.pos_sec_main');
+const postsContainer = document.getElementById('posts-main');
+const loading = document.querySelector('.circle-main');
 const filter = document.getElementById('filter');
 let limit = 5;
 let page = 1;
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+  const data = await res.json();
+  return data;
+}
+async function showPosts() {
+  const posts = await getPosts();
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+    postsContainer.appendChild(postEl);
+  });
+}
+function showLoading() {
+  loading.classList.add('show');
+  setTimeout(() => {
+    loading.classList.remove('show');
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+showPosts();
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+filter.addEventListener('input', filterPosts);
